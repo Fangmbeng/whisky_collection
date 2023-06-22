4ece2e8a463805e5d8c27e9e78d32c521c0f1b68
Fangmbeng <yorobeau@gmail.com>
2023-06-22 14:53:10 -0600

    Fangmbeng

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -81,11 +81,11 @@ def delete_post(post_id):
     return redirect(url_for('site.index'))
 
 #routes for JSON request
-from app.blueprints.authentication.auth import basic_auth, token_auth
+#from app.blueprints.authentication.auth import basic_auth, token_auth
 
 
 @api.route('/token')
-@basic_auth.login_required
+#@basic_auth.login_required
 def index_():
     user = basic_auth.current_user()
     token = user.get_token()
@@ -102,7 +102,7 @@ def getpost(post_id):
     return posts.to_dict()
 
 @api.route('/posts', methods=['POST'])
-@token_auth.login_required
+#@token_auth.login_required
 def createpost():
     if not request.is_json:
         return("your request content-type is not JSON"), 400
@@ -143,7 +143,7 @@ def createuser():
     return new_user.to_dict(), 201
     
 @api.route('/post/edit/<int:post_id>', methods=['POST'])
-@token_auth.login_required
+#@token_auth.login_required
 def editpost(post_id):
     post = Post.query.get(post_id)
     if not request.is_json:
@@ -161,7 +161,7 @@ def editpost(post_id):
 
 
 @api.route('/post/delete/<int:post_id>', methods=['POST'])
-@token_auth.login_required
+#@token_auth.login_required
 def deletepost(post_id):
     post = Post.query.get(post_id)
     if not request.is_json:
